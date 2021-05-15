5d0f4b8232864a477b0c24231de146788417bf1b
秋雨落 <i@rain.cx>
2021-05-15 14:56:09 +0800

    :tada: Init, sitll WIP
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ Spigot2IntermediaryTool/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Spigot2IntermediaryTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("This is a spigot to intermediary mappings generate tool.");
+                Console.WriteLine("Usage: dotnet Spigot2IntermediaryTool.dll <minecraftVersion>");
+                return;
+            }
+
+            var minecraftVersion = args[0];
+            
+            // Todo: Download Spigot build data and fabric intermediary.
+            
+            var workingPath = Directory.GetCurrentDirectory();
+            var bukkitClassesPath = $"{workingPath}/bukkit-{minecraftVersion}-cl.csrg";
+            var bukkitMembersPath = $"{workingPath}/bukkit-{minecraftVersion}-members.csrg";
+            var intermediaryPath = $"{workingPath}/{minecraftVersion}.tiny";
+            if (!File.Exists(bukkitClassesPath))
+            {
+                Console.WriteLine("F: Bukkit class mapping is not found.");
+                return;
+            }
+
+            if (!File.Exists(bukkitMembersPath))
+            {
+                Console.WriteLine("F: Bukkit member mapping is not found.");
+                return;
+            }
+            
+            if (!File.Exists(intermediaryPath))
+            {
+                Console.WriteLine("F: Intermediary mapping is not found.");
+                return;
+            }
+
+            var bukkitClasses = File.ReadAllLines(bukkitClassesPath);
+            var bukkitMembers = File.ReadAllLines(bukkitMembersPath);
+            var intermediary = File.ReadAllLines(intermediaryPath);
+
+            new Spigot2Intermediary(bukkitClasses, bukkitMembers, intermediary).Run();
+        }
+    }
+}
---
+++ Spigot2IntermediaryTool/Spigot2Intermediary.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spigot2IntermediaryTool
+{
+    public class Spigot2Intermediary
+    {
+        private string[] BukkitClasses { get; }
+        private string[] BukkitMembers { get; }
+        private string[] Intermediary { get; }
+
+        private Dictionary<string, string> BukkitToMojang { get; } = new();
+        private Dictionary<string, string> MojangToBukkit { get; } = new();
+        
+        private Dictionary<string, string> IntermediaryClasses { get; } = new();
+        // private Dictionary<string, string> IntermediaryFields { get; } = new();
+        // private Dictionary<string, string> IntermediaryMethods { get; } = new();
+        
+        public Spigot2Intermediary(string[] bukkitClasses, string[] bukkitMembers, string[] intermediary)
+        {
+            BukkitClasses = bukkitClasses;
+            BukkitMembers = bukkitMembers;
+            Intermediary = intermediary;
+        }
+
+        public void Run()
+        {
+            LoadBukkit();
+            LoadIntermediary();
+
+            MakeTiny();
+        }
+
+        private void LoadBukkit()
+        {
+            Console.WriteLine("I: Loading bukkit classes.");
+            foreach (var classLine in BukkitClasses)
+            {
+                if (!classLine.StartsWith("#"))
+                {
+                    var classMojangToBukkit = classLine.Split(" ");
+                    Console.WriteLine($"D: Loaded bukkit {classMojangToBukkit[1]}.");
+                    BukkitToMojang[classMojangToBukkit[1]] = classMojangToBukkit[0];
+                    MojangToBukkit[classMojangToBukkit[0]] = classMojangToBukkit[1];
+                }
+            }
+        }
+        
+        private void LoadIntermediary()
+        {
+            Console.WriteLine("I: Loading intermediary mappings.");
+            foreach (var intermediaryLine in Intermediary)
+            {
+                if (intermediaryLine.StartsWith("CLASS"))
+                {
+                    var classParts = intermediaryLine.Split("\t");
+                    Console.WriteLine($"D: Loaded intermediary {classParts[2]}.");
+                    IntermediaryClasses[classParts[1]] = classParts[2];
+                }
+            }
+        }
+        
+        private void MakeTiny()
+        {
+            Console.WriteLine("I: Making tiny.");
+            var tiny = new List<string>();
+            tiny.Add("v1\tbukkit\tbukkitIntermediary");
+
+            foreach (var intermediaryLine in Intermediary)
+            {
+                if (intermediaryLine.StartsWith("CLASS"))
+                {
+                    var classDescription = intermediaryLine.Split("\t");
+
+                    var mojang = classDescription[1];
+                    if (MojangToBukkit.ContainsKey(mojang))
+                    {
+                        tiny.Add($"CLASS\t{MojangToBukkit[mojang]}\t{IntermediaryClasses[mojang]}");
+                    }
+                }
+            }
+
+            foreach (var t in tiny)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
